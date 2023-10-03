3ebfeba0102268081a1912faad477887b29481fc
dima <intredford@gmail.com>
2023-10-03 18:32:32 +0800

    feat: переделана логика разбивки постов по страницам

--- server/server.js
+++ server/server.js
@@ -1,25 +1,31 @@
 import express from 'express';
 import { loadPosts } from './posts.js';
 
+
 const app = express();
 const port = 3000;
-const postsPerPage = 5;
-const posts = loadPosts();
-
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
-app.get('/', (req, res) => {
+const postsPerPage = 5;
+const posts = loadPosts();
+const pages = Array.from(
+	{ length: Math.ceil(posts.length / postsPerPage) },
+	(_, i) => posts.slice(i * postsPerPage, (i + 1) * postsPerPage)
+).reverse();
+
+app.get('/', (req, res) =>
+{
 	const totalPages = Math.ceil(posts.length / postsPerPage);
 	const page = parseInt(req.query.page) || totalPages;
-	const pageNumbers = Array.from({ length: totalPages }, (_, index) => totalPages - index);
-	const startIndex = (page - 1) * postsPerPage;
-	const endIndex = startIndex + postsPerPage;
-	const pagePosts = posts.slice(startIndex, endIndex);
+	const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1).reverse();
+
+	const pagePosts = pages[page-1]
 
 	res.render('layout', { posts: pagePosts, page, pageNumbers, totalPages });
 });
 
-app.listen(port, () => {
+app.listen(port, () =>
+{
 	console.log(`Blog server is running on port ${port}`);
 });
