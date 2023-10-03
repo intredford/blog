e54dad9b1c4128eb9d65714dee5759a2040a67d5
dima <intredford@gmail.com>
2023-10-03 13:35:33 +0800

    fix: задана переменная postsDirectory

--- server/posts.js
+++ server/posts.js
@@ -8,6 +8,8 @@ const md = markdownIt({
     // quotes: '«»„“'
 });
 
+const postsDirectory = './posts'
+
 // Парсит дату из названий файлов постов: 29-09-23
 function parseDate(dateStr) {
     const [day, month, year] = dateStr.split('-').map(Number);
@@ -27,11 +29,10 @@ function getFormattedDate(dateStr) {
 }
 
 export function loadPosts() {
-    return fs.readdirSync('./posts') // прочитать посты
+    return fs.readdirSync(postsDirectory) // прочитать посты
         .map(postFile => {
             const markdown = fs.readFileSync(`${postsDirectory}/${postFile}`, 'utf8');
             const html = md.render(markdown); // md -> html
-            
             const dateStr = postFile.split('_')[0];
             const { date, formattedDate } = getFormattedDate(dateStr)
 
